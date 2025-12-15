356c4331b887b069c69b44788425768a778ff0cb
tt_41 <ti.discretedev@gmail.com>
2025-12-15 18:01:10 +0100

    Ready for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. ADD YOUR DOMAIN HERE
+  site: 'https://tim-dscrtdv.com', 
+  
+  // 2. Base should be '/' since you are at the root of the domain
+  base: '/',
+
   integrations: [react()],
 
   vite: {
